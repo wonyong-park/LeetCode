42dc8929fcb12d2a5c1d455772674584d972d683
wonyong-park <59985098+wonyong-park@users.noreply.github.com>
2021-07-12 20:58:03 +0900

    Time: 1 ms (99.94%), Space: 43 MB (69.87%) - LeetHub

--- counting-bits/counting-bits.java
+++ counting-bits/counting-bits.java
@@ -1,9 +1,14 @@
 class Solution {
     public int[] countBits(int n) {
         int arr[] = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            arr[i] = Integer.bitCount(i);
+        int standard = 1;
+        for (int i = 1; i <= n ; i++) {
+            if(standard * 2 == i){
+                standard = i;
+            }
+            arr[i] = arr[i - standard] + 1;
         }
+
         return arr;
     }
 }
