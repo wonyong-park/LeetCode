394d4eb645604445fd0c68637c0108d96a5ee12c
wonyong-park <59985098+wonyong-park@users.noreply.github.com>
2021-07-20 00:53:22 +0900

    Time: 0 ms (100.00%), Space: 39.5 MB (33.46%) - LeetHub

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -1,35 +1,31 @@
 class Solution {
-    static List<List<Integer>> result;
-
-    public List<List<Integer>> permute(int[] nums) {
-        result = new ArrayList<>();
-        //방문한것을 확인하는 변수
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
         boolean[] visited = new boolean[nums.length];
-        //방문하면서 차례로 담을 변수
         int[] output = new int[nums.length];
-        perm(nums, output, 0, visited);
+        int n = nums.length;
+
+        perm(result, nums, output, visited, 0, n);
 
         return result;
     }
 
-    static void perm(int[] nums, int[] output, int depth, boolean[] visited){
-
-        if(depth == nums.length){
-            result.add(Arrays.stream(output).boxed().collect(Collectors.toList()));
+    static void perm(List<List<Integer>> result, int[]arr, int[] output, boolean[] visited, int depth, int n) {
+        if (depth == n) {
+            List<Integer> dump = new ArrayList<>();
+            for (Integer i : output){
+                dump.add(i);
+            }
+            result.add(dump);
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            //i번째가 방문 안한경우
-            if(visited[i] != true){
-                //방문 처리
+        for (int i = 0; i < n; i++) {
+            if (visited[i] != true) {
                 visited[i] = true;
-                //방문 값 입력
-                output[depth] = nums[i];
-                perm(nums,output,depth+1,visited);
-                //방문이 끝나면
+                output[depth] = arr[i];
+                perm(result, arr, output, visited, depth + 1, n);
                 visited[i] = false;
             }
         }
-
     }
 }
