6764b76c6afe04584e4da2f932897c8f215cabae
wonyong-park <59985098+wonyong-park@users.noreply.github.com>
2021-08-23 19:55:11 +0900

    Time: 9 ms (83.55%), Space: 41.3 MB (87.93%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.java
+++ top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,30 +1,31 @@
 import java.util.*;
-import static java.util.Map.*;
 
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> hm = new HashMap<>();
+    public int[] topKFrequent(int[] nums, int k){
+        HashMap<Integer, Integer> hm = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            if(hm.containsKey(nums[i])){
-                hm.put(nums[i], hm.get(nums[i]) + 1);
-            }else{
-                hm.put(nums[i], 1);
-            }
+            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
         }
 
-        List<Entry<Integer, Integer>> list = new ArrayList<>(hm.entrySet());
-        Collections.sort(list, new Comparator<Entry<Integer, Integer>>() {
+        int[][] arr = new int[hm.size()][2];
+        int i = 0;
+        for (int num : hm.keySet()) {
+            arr[i][0] = num;
+            arr[i][1] = hm.get(num);
+            i++;
+        }
+        
+        Arrays.sort(arr, new Comparator<int[]>() {
             @Override
-            public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
-                return o2.getValue() -  o1.getValue();
+            public int compare(int[] o1, int[] o2) {
+                return o2[1]-o1[1];
             }
         });
 
         int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = list.get(i).getKey();
-
+        for (int j = 0; j < k; j++) {
+            result[j] = arr[j][0];
         }
 
         return result;
