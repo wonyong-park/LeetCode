3443780a362f4d9b29a5112e4aa910e5482f47e0
wonyong-park <59985098+wonyong-park@users.noreply.github.com>
2021-08-30 17:17:45 +0900

    Time: 0 ms (100.00%), Space: 39 MB (63.96%) - LeetHub

---
+++ rotate-image/rotate-image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = matrix.length-1; j > matrix.length/2-1 ; j--) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-j-1];
+                matrix[i][matrix.length-j-1] = temp;
+            }
+        }
+
+    }
+}
