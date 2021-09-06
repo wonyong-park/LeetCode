c9bf9b3d121bf1d19d8d53b1b0622f423f662681
wonyong-park <59985098+wonyong-park@users.noreply.github.com>
2021-09-06 11:13:34 +0900

    Time: 6 ms (83.44%), Space: 42 MB (76.63%) - LeetHub

--- group-anagrams/group-anagrams.java
+++ group-anagrams/group-anagrams.java
@@ -1,42 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        boolean[] visted = new boolean[strs.length];
+        if(strs.length == 0 || strs == null) return new ArrayList<>();
+
+        HashMap<String, ArrayList<String>> hs = new HashMap<>();
         List<List<String>> result = new ArrayList<>();
 
         for (int i = 0; i < strs.length; i++) {
-            if(!visted[i]){
-                List<String> list = new ArrayList<>();
-                String str = strs[i];
-                list.add(str);
-                visted[i] = true;
+            char[] sArr = strs[i].toCharArray();
+            Arrays.sort(sArr);
+            String str = String.valueOf(sArr);
 
-                for (int j = i+1; j < strs.length; j++) {
-                    String str2 = strs[j];
-                    //같은 경우
-                    if(anagrams(str, str2)){
-                        list.add(str2);
-                        visted[j] = true;
-                    }
-                }
-
-                result.add(list);
+            if (!hs.containsKey(str)) {
+                hs.put(str, new ArrayList<>());
             }
+
+            hs.get(str).add(strs[i]);
+        }
+
+        for (Map.Entry<String, ArrayList<String>> stringArrayListEntry : hs.entrySet()) {
+            result.add(stringArrayListEntry.getValue());
         }
 
         return result;
-    }
-
-    private boolean anagrams(String s1, String s2) {
-        if (s1.length() != s2.length()) {
-            return false;
-        }
-
-        char[] s1Array = s1.toCharArray();
-        char[] s2Array = s2.toCharArray();
-
-        Arrays.sort(s1Array);
-        Arrays.sort(s2Array);
-
-        return Arrays.equals(s1Array, s2Array);
     }
 }
