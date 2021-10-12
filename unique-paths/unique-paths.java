d1a368ad77c2b71ad7262fdc9de344b72e0f7dcf
wonyong-park <59985098+wonyong-park@users.noreply.github.com>
2021-10-12 18:40:35 +0900

    Time: 1 ms (27.75%), Space: 38 MB (10.08%) - LeetHub

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -3,19 +3,15 @@ class Solution {
         int[][] arr = new int[m][n];
 
         for (int i = 0; i < arr.length; i++) {
-            arr[i][0] = 1;
-        }
+            for (int j = 0; j < arr[0].length; j++) {
+                if (i == 0 || j == 0) {
+                    arr[i][j] = 1;
+                }else{
+                    arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
 
-        for (int i = 0; i < arr[0].length; i++) {
-            arr[0][i] = 1;
-        }
-
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = 1; j < arr[0].length; j++) {
-                arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+                }
             }
         }
-
         return arr[m - 1][n - 1];
     }
 }
