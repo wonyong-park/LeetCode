85804d102e6118c047725158d79d7dd582f28b5c
wonyong-park <59985098+wonyong-park@users.noreply.github.com>
2021-11-03 16:28:11 +0900

    Time: 1 ms (91.27%), Space: 38.8 MB (56.79%) - LeetHub

--- trapping-rain-water/trapping-rain-water.java
+++ trapping-rain-water/trapping-rain-water.java
@@ -2,22 +2,29 @@ class Solution {
     public int trap(int[] height) {
         int total = 0;
 
+        //인덱스의 위치에서 봤을때 왼쪽 가장 긴 높이와 오른쪽 가장 긴 높이
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
+
+        int max = 0;
+        for (int i = 0; i < height.length - 1; i++) {
+            max = Math.max(max, height[i]);
+            left[i + 1] = max;
+        }
+
+        max = 0;
+        for (int i = left.length-2; i >= 0; i--) {
+            max = Math.max(max, height[i + 1]);
+            right[i] = max;
+        }
+
         for (int i = 0; i < height.length; i++) {
-            int minHeight = Math.min(calcMax(height,0, i), calcMax(height, i + 1, height.length));
+            int minHeight = Math.min(left[i], right[i]);
             if (minHeight - height[i] > 0) {
                 total += minHeight - height[i];
             }
         }
 
         return total;
-    }
-
-    private int calcMax(int[] height, int start, int end) {
-        int max = 0;
-        for (int i = start; i < end; i++) {
-            max = Math.max(max, height[i]);
-        }
-
-        return max;
     }
 }
